67074fc4d6aff1bb1bbfb4b21787d2eae5d3d6cf
Dustin Specker <DustinSpecker@DustinSpecker.com>
2014-09-21 18:54:07 -0500

    add basic routes for mockUsers

--- server.js
+++ server.js
@@ -2,13 +2,70 @@
 
 var good = require('good')
   , hapi = require('hapi')
-  , server = new hapi.Server(3000);
+  , joi = require('joi')
+  , server = new hapi.Server(3000)
+  , mockUsers = [
+    {
+      name: 'abe'
+    }, {
+      name: 'mary'
+    }, {
+      name: 'todd'
+    }, {
+      name: 'lincoln'
+    }
+  ];
 
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
     reply('Hello, world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/mockUsers/{id?}',
+  handler: function (request, reply) {
+    if (request.params.id) {
+      if (mockUsers.length <= request.params.id) {
+        return reply('No mock user found.').code(404);
+      }
+      return reply(mockUsers[request.params.id]);
+    }
+    reply(mockUsers);
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/mockUsers',
+  config: {
+    handler: function (request, reply) {
+      var newMockUser = {
+        name: request.payload.name
+      };
+      mockUsers.push(newMockUser);
+      reply(newMockUser);
+    },
+    validate: {
+      payload: {
+        name: joi.string().required().min(3).max(10)
+      }
+    }
+  }
+});
+
+server.route({
+  method: 'DELETE',
+  path: '/mockUsers/{id}',
+  handler: function (request, reply) {
+    if (mockUsers.length <= request.params.id) {
+      return reply('No mock user found.').code(404);
+    }
+    mockUsers.splice(request.params.id, 1);
+    reply(true);
   }
 });
 
